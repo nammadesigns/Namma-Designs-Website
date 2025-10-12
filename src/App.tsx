ead88d6f3520ea27a8902ec103ccc059c9fa388a
NagarajaAcharya1 <nagarajaacharya517@gmail.com>
2025-10-12 19:41:03 +0530

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -20,6 +21,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Analytics />
+      <SpeedInsights />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
